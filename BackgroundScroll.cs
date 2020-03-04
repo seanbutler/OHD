f92bc87335420322049af1ccefa870779f2d5b96
sean butler <seanbutler@users.noreply.github.com>
2020-03-04 12:59:19 +0000

    Update BackgroundScroll.cs

--- BackgroundScroll.cs
+++ BackgroundScroll.cs
@@ -1,29 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class BackgroundScroll : MonoBehaviour
-{
 
 //     INSERT VARIABLES 1 HERE
 
     public float speed = -1.0f;
     public float leftHandEdge = -10.0f;
     public float rightHandEdge = 10.0f;
-
     private float verticalOffset = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        // INSERT RESET 3 CODE HERE
-        verticalOffset = transform.position.y;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-// INSERT SCROLL CODE HERE
+
+    // INSERT SCROLL CODE HERE
 
         float realSpeed = speed * Time.deltaTime;
         transform.position += new Vector3(realSpeed, 0.0f, 0.0f);
@@ -35,17 +21,13 @@ public class BackgroundScroll : MonoBehaviour
         {
             ResetPosition();
         }
-    }
-
 
     // INSERT RESET 2 CODE HERE
 
     void ResetPosition()
     {
-
-        // INSERT RESET 2 CODE HERE
-
         transform.position = new Vector3(rightHandEdge, verticalOffset, 0.0f);
     }
 
-}
+    // INSERT RESET 3 CODE HERE
+    verticalOffset = transform.position.y;
